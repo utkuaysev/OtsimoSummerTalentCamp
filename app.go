498917414620213bf90d93a6f714021f51eed826
Utku Aysev <utkua557@gmail.com>
2020-12-17 01:45:00 +0300

    app.go is created for create just one db connection and use it in main program

---
+++ app.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	//"go.mongodb.org/mongo-driver/bson"
+)
+
+var client *mongo.Client
+var err error
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// Connect to MongoDB
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+}
+func end() {
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+
+}
--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"time"
-
-	//"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// You will be using this Trainer type later in the program
 type Candidate struct {
 	_id        string //Unique hash that identifies candidate.
 	first_name string //First name of the candidate.
@@ -40,32 +32,9 @@ type Candidate struct {
 }
 
 func main() {
-	// Rest of the code will go here
-	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB!")
-
-	//collection := client.Database("Otsimo").Collection("Candidates")
-	err = client.Disconnect(context.TODO())
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connection to MongoDB closed.")
 
 }
+
+//func ReadCandidate (_id string) (Candidate, error){
+//
+//}
