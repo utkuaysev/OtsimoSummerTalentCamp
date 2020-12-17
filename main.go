2c5a19470307a93269ff3e2a09b3c2deed92b243
Utku Aysev <utkua557@gmail.com>
2020-12-17 15:41:06 +0300

    Complete meeting function is done

--- main.go
+++ main.go
@@ -29,9 +29,11 @@ func init() {
 }
 func main() {
 	//ReadCandidate("5b758c6151d9590001def630")
-	var asd = (time.Now())
-	var asd2 = &asd
-	ArrangeMeeting("5b75820a51d9590001def61e", asd2)
+	/*	var time_check = (time.Now())
+		var time_check_pointer = &time_check
+		ArrangeMeeting("5b75820a51d9590001def61e", time_check_pointer)
+	*/
+	//	CompleteMeeting("5b75881051d9590001def62a")
 	defer end()
 	defer f.Close()
 }
@@ -61,12 +63,15 @@ func DeleteCandidate(_id string) error {
 	log.Println("Deleted with id %v documents in the trainers collection\n", _id)
 	return err
 }
-func ArrangeMeeting(_id string, nextMeetingTime *time.Time) error {
+func is_next_meeting_null(_id string) bool {
 	candidate, err := ReadCandidate(_id)
 	if err != nil {
-		return err
+		return true
 	}
-	if !candidate.Next_meeting.IsZero() {
+	return !candidate.Next_meeting.IsZero()
+}
+func ArrangeMeeting(_id string, nextMeetingTime *time.Time) error {
+	if !is_next_meeting_null(_id) {
 		err1 := fmt.Errorf("There is a meeting has not completed for id %s.You can not arrange new meeting", _id)
 		log.Println(err1)
 		return err1
@@ -74,15 +79,25 @@ func ArrangeMeeting(_id string, nextMeetingTime *time.Time) error {
 	filter := bson.D{{"_id", _id}}
 
 	update := bson.D{
-		{"$inc", bson.D{
-			{"meeting_count", 1},
-		}}, {"$set", bson.D{{"next_meeting", nextMeetingTime}}},
+		{"$inc", bson.D{{"meeting_count", 1}}}, {"$set", bson.D{{"next_meeting", nextMeetingTime}}},
 	}
 	updateResult, err := candidates_collection.UpdateOne(context.TODO(), filter, update)
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+	return err
+}
+func CompleteMeeting(_id string) error {
 
+	filter := bson.D{{"_id", _id}}
+	update := bson.D{
+		{"$set", bson.D{{"next_meeting", nil}}},
+	}
+	updateResult, err := candidates_collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
 	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
 	return err
 }
