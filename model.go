d8cb7ef3f4c6fb66117634a6ad97f45767142607
Utku Aysev <utkua557@gmail.com>
2020-12-17 23:55:07 +0300

    structs functions is added for candidate

--- model.go
+++ model.go
@@ -35,3 +35,18 @@ type Candidate struct {
 	Next_meeting  time.Time //Timestamp of the next meeting between the Otsimo team and the candidate.
 	Assignee      string    //The id of the Otsimo team member who is responsible for this candidate.
 }
+
+func (candidate Candidate) is_next_meeting_null() bool {
+	return candidate.Next_meeting.IsZero()
+}
+func (candidate Candidate) is_max_number_of_meeting_reached() bool {
+	return candidate.Meeting_count == 4
+}
+
+func (candidate Candidate) is_last_meeting_arranging() bool {
+	return candidate.Meeting_count == 3
+}
+
+func (candidate Candidate) get_name() string {
+	return candidate.First_name + " " + candidate.Last_name
+}
